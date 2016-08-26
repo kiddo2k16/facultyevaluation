54302a5f3237bdf482e72843f16f6ed2306b8120
kiddo2k16 <kiddo2k16@gmail.com>
2016-08-26 15:10:22 +0800

    update

--- application/views/themes/default.php
+++ application/views/themes/default.php
@@ -57,6 +57,7 @@ foreach($css_files as $file): ?>
 
     <!--  styles -->
     <link href="<?php echo base_url(); ?>/public/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="<?php echo base_url(); ?>/public/css/mob-st.css" rel="stylesheet" media="screen">
 	 <script src="<?php echo base_url() ?>/public/js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo base_url() ?>/public/js/bootstrap.min.js"></script>
